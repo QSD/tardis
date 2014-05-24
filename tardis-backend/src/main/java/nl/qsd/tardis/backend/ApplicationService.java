cd2fc327dbc93626130b6351614728ed27227838
Erik Mulder <erik.mulder@qsd.nl>
2014-05-24 15:29:08 +0200

    dropwizard guice integration

--- tardis-backend/src/main/java/nl/qsd/tardis/backend/ApplicationService.java
+++ tardis-backend/src/main/java/nl/qsd/tardis/backend/ApplicationService.java
@@ -3,27 +3,34 @@ package nl.qsd.tardis.backend;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import nl.qsd.tardis.backend.status.StatusResource;
+
+import com.hubspot.dropwizard.guice.GuiceBundle;
 
 public class ApplicationService extends Application<TardisConfiguration> {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("ping");
-        new ApplicationService().run(args);
-    }
+	public static void main(String[] args) throws Exception {
+		System.out.println("ping");
+		new ApplicationService().run(args);
+	}
 
-    @Override
-    public void initialize(Bootstrap<TardisConfiguration> tardisConfigurationBootstrap) {
+	@Override
+	public void initialize(
+			Bootstrap<TardisConfiguration> bootstrap) {
+		GuiceBundle<TardisConfiguration> guiceBundle = GuiceBundle
+				.<TardisConfiguration> newBuilder()
+				// TODO: add module
+				.addModule(new HelloWorldModule())
+				.setConfigClass(TardisConfiguration.class).build();
 
-        System.out.println("tardis initialize ");
-    }
+		bootstrap.addBundle(guiceBundle);
+	}
 
-    @Override
-    public void run(TardisConfiguration tardisConfiguration, Environment environment) throws Exception {
+	@Override
+	public void run(TardisConfiguration tardisConfiguration,
+			Environment environment) throws Exception {
+		System.out.println("tardis service ");
+		// final Thingy thingy = config.getThingyFactory().build();
+		// environment.jersey().register(new ThingyResource(thingy));
 
-        System.out.println("tardis service ");
-//        final Thingy thingy = config.getThingyFactory().build();
-//        environment.jersey().register(new ThingyResource(thingy));
-
-    }
+	}
 }
