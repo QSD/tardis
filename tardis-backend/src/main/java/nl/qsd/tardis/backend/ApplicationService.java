d1fdf55d1a6d9bb1fb02976d818ae44402e08d37
jeroen wijdemans <jeroen.wijdemans@gmail.com>
2014-05-24 15:06:55 +0200

    enabling runnable bootstrap jar file

--- tardis-backend/src/main/java/nl/qsd/tardis/backend/ApplicationService.java
+++ tardis-backend/src/main/java/nl/qsd/tardis/backend/ApplicationService.java
@@ -8,17 +8,20 @@ import nl.qsd.tardis.backend.status.StatusResource;
 public class ApplicationService extends Application<TardisConfiguration> {
 
     public static void main(String[] args) throws Exception {
+        System.out.println("ping");
         new ApplicationService().run(args);
     }
 
     @Override
     public void initialize(Bootstrap<TardisConfiguration> tardisConfigurationBootstrap) {
 
+        System.out.println("tardis initialize ");
     }
 
     @Override
     public void run(TardisConfiguration tardisConfiguration, Environment environment) throws Exception {
 
+        System.out.println("tardis service ");
 //        final Thingy thingy = config.getThingyFactory().build();
 //        environment.jersey().register(new ThingyResource(thingy));
 
