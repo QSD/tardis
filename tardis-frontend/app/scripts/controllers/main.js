6a91519c9d8ad033d6e1275d6e7f28bfdb5169b3
Alex Kuiper <alex@nightwhistler.net>
2014-05-24 14:58:27 +0200

    Added directive

--- tardis-frontend/app/scripts/controllers/main.js
+++ tardis-frontend/app/scripts/controllers/main.js
@@ -4,7 +4,24 @@ angular.module('QSD.Tardis')
 
   .controller('MainCtrl', function($scope, $location, version) {
 
+    console.log( "Inside main controller");
+
     $scope.$path = $location.path.bind($location);
     $scope.version = version;
+
+    $scope.lights = [
+        {
+            name: "Aap",
+            colour: "green"
+        },
+        {
+            name: "Noot",
+            colour: "orange"
+        },
+        {
+            name: "Mies",
+            colour: "red"
+        }
+    ];
 
   });
